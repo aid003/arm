25b530895468ad8cc7140e9d40a0f43fd189dd5c
aid003 <teremochek859@gmail.com>
2022-12-13 04:51:05 +0300

    noscroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
+let body = document.querySelector('body'),
+    count = 0;
+
 document.querySelector('.burger').addEventListener('click', function () {
     document.querySelector('.menu').classList.toggle("animate");
+    body.style.overflowY = 'hidden';
+    ++count;
+    if (count == 2) {
+        body.style.overflowY = 'visible';
+        count = 0;
+    }
 });
+
