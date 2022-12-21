f00e362df8aab158516b145c7e3d6e53a562953d
aid003 <teremochek859@gmail.com>
2022-12-21 03:26:44 +0300

    21.12 js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
-let body = document.querySelector('body'),
-    count = 0;
+document.addEventListener('DOMContentLoaded', function() {
 
-document.querySelector('.burger').addEventListener('click', function () {
-    document.querySelector('.menu').classList.toggle("animate");
-    body.style.overflowY = 'hidden';
-    ++count;
-    if (count == 2) {
-        body.style.overflowY = 'visible';
-        count = 0;
-    }
-});
+    document.addEventListener('click', function () {
+        
+        // burger menu 
+        document.querySelector('.burger').addEventListener('click', function () {
+            document.querySelector('.menu').classList.toggle("animate")
+        });
+
+    });
+})
 
